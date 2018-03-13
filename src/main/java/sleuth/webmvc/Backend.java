d767bb02be44349c98832bea075af5dc01cc2018
Adrian Cole <acole@pivotal.io>
2018-03-13 15:14:49 +0800

    WIP Kafka example

--- src/main/java/sleuth/webmvc/Backend.java
+++ src/main/java/sleuth/webmvc/Backend.java
@@ -1,17 +1,33 @@
 package sleuth.webmvc;
 
-import java.util.Date;
+import brave.Span;
+import brave.kafka.clients.KafkaTracing;
+import brave.propagation.CurrentTraceContext;
+import brave.propagation.CurrentTraceContext.Scope;
+import java.lang.reflect.Method;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.sleuth.util.SpanNameUtil;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Import;
+import org.springframework.kafka.annotation.KafkaListener;
+
+import static org.springframework.util.ReflectionUtils.findMethod;
 
 @EnableAutoConfiguration
-@RestController
+@EnableAspectJAutoProxy
+@Import(TracingAspect.class)
 public class Backend {
 
-  @RequestMapping("/api") public String printDate() {
-    return new Date().toString();
+  @KafkaListener(topics = "backend")
+  public void onMessage(ConsumerRecord<?, ?> message) {
+    System.err.println(message);
   }
 
   public static void main(String[] args) {
@@ -19,5 +35,36 @@ public class Backend {
         "--spring.application.name=backend",
         "--server.port=9000"
     );
+  }
+}
+
+@Aspect class TracingAspect {
+  @Autowired CurrentTraceContext currentTraceContext;
+  @Autowired KafkaTracing kafkaTracing;
+
+  // toy example as listeners allow parameters besides record.
+  // Real needs to intercept MessagingMessageListenerAdapter.invokeHandler or similar
+  @Around("@annotation(listener) && args(record)")
+  public Object traceListener(
+      ProceedingJoinPoint pjp,
+      KafkaListener listener,
+      ConsumerRecord<?, ?> record
+  ) throws Throwable {
+    String name = SpanNameUtil.toLowerHyphen(getMethod(pjp).getName());
+    Span span = kafkaTracing.nextSpan(record).name(name).start();
+
+    try (Scope scope = currentTraceContext.newScope(span.context())) {
+      return pjp.proceed();
+    } catch (RuntimeException | Error e) {
+      span.error(e);
+      throw e;
+    } finally {
+      span.finish();
+    }
+  }
+
+  static Method getMethod(ProceedingJoinPoint pjp) {
+    Method method = ((MethodSignature) pjp.getSignature()).getMethod();
+    return findMethod(pjp.getTarget().getClass(), method.getName(), method.getParameterTypes());
   }
 }
--- src/main/java/sleuth/webmvc/Frontend.java
+++ src/main/java/sleuth/webmvc/Frontend.java
@@ -1,29 +1,23 @@
 package sleuth.webmvc;
 
+import java.util.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @EnableAutoConfiguration
 @RestController
 @CrossOrigin // So that javascript can be hosted elsewhere
 public class Frontend {
 
-  @Autowired RestTemplate restTemplate;
+  @Autowired KafkaTemplate<String, String> kafkaTemplate;
 
-  String backendBaseUrl = System.getProperty("spring.example.backendBaseUrl", "http://localhost:9000");
-
-  @RequestMapping("/") public String callBackend() {
-    return restTemplate.getForObject(backendBaseUrl + "/api", String.class);
-  }
-
-  @Bean RestTemplate restTemplate() {
-    return new RestTemplate();
+  @RequestMapping("/") public void callBackend() {
+    kafkaTemplate.send("backend", new Date().toString());
   }
 
   public static void main(String[] args) {
